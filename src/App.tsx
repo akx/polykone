2f193aa84a5631804b2afd92c975c2ecebc724ea
Aarni Koskela <akx@iki.fi>
2024-03-14 16:50:24 +0200

    Refine

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React from "react";
 export default function App() {
   const imgRef = React.useRef<HTMLImageElement>(null);
   const [img, setImg] = React.useState<string>();
+  const [tool, setTool] = React.useState<"draw" | "move">("draw");
+  const [movingIndex, setMovingIndex] = React.useState<number | null>(null);
   const [points, setPoints] = React.useState<[number, number][]>([]);
   const loadBlob = (e: React.ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
@@ -16,42 +18,97 @@ export default function App() {
     const y = e.clientY - offset?.top;
     setPoints((points) => [...points, [x, y]]);
   };
+  const handleMove = function (e: React.MouseEvent) {
+    if (movingIndex === null) {
+      return;
+    }
+    const snap = e.shiftKey ? 10 : 1;
+    const offset = e.currentTarget?.getBoundingClientRect();
+    const x = Math.round((e.clientX - offset?.left) / snap) * snap;
+    const y = Math.round((e.clientY - offset?.top) / snap) * snap;
+    setPoints((points) => {
+      const newPoints = [...points];
+      newPoints[movingIndex] = [x, y];
+      return newPoints;
+    });
+  };
 
   return (
     <div className="App">
-      Choose image file:
-      <input type="file" accept="image/*" onChange={loadBlob} />
-      <br />
-      <button onClick={() => setPoints([])}>Clear</button>
-      <button onClick={() => setPoints((points) => [...points.slice(0, -1)])}>
-        Undo
-      </button>
-      <br />
-      <textarea value={JSON.stringify(points)} readOnly />
-      <hr />
+      <header>
+        <div>
+          Choose image file:
+          <input type="file" accept="image/*" onChange={loadBlob} />
+        </div>
+        <button
+          onClick={() => setTool("draw")}
+          style={{ fontWeight: tool === "draw" ? "bold" : undefined }}
+        >
+          Draw
+        </button>
+        <button
+          onClick={() => setTool("move")}
+          style={{ fontWeight: tool === "move" ? "bold" : undefined }}
+        >
+          Move
+        </button>
+        <div>&middot;</div>
+        <button
+          onClick={() =>
+            points.length && confirm("Clear?") ? setPoints([]) : void 8
+          }
+        >
+          Clear
+        </button>
+        <button onClick={() => setPoints((points) => [...points.slice(0, -1)])}>
+          Undo
+        </button>
+        <div className="spacer"></div>
+        <textarea value={JSON.stringify(points)} readOnly />
+      </header>
       <div style={{ position: "relative" }}>
         <img
-          style={{ position: "absolute" }}
+          style={{
+            position: "absolute",
+            cursor: tool == "draw" ? "crosshair" : undefined,
+          }}
           ref={imgRef}
           src={img}
           hidden={!img}
-          onClick={addPoint}
+          onClick={tool === "draw" ? addPoint : undefined}
         />
         <svg
           width={imgRef.current?.width}
           height={imgRef.current?.height}
-          style={{ position: "absolute", pointerEvents: "none" }}
+          style={{
+            position: "absolute",
+            pointerEvents: tool == "draw" ? "none" : undefined,
+          }}
+          onMouseMove={tool === "move" ? handleMove : undefined}
+          onMouseUp={() => setMovingIndex(null)}
         >
           <polygon
             points={points.map(([x, y]) => `${x},${y}`).join(" ")}
-            fill="red"
+            fill="cyan"
             stroke="black"
             strokeDasharray="10, 10"
             strokeWidth={3}
             fillOpacity={0.2}
+            style={{ pointerEvents: "none" }}
           />
           {points.map(([x, y], i) => (
-            <circle cx={x} cy={y} r={5} key={i} fill="red" />
+            <circle
+              cx={x}
+              cy={y}
+              r={7}
+              key={i}
+              fill={tool == "move" ? "lime" : "red"}
+              stroke={tool == "move" ? "black" : undefined}
+              style={{ cursor: "move" }}
+              onMouseDown={
+                tool === "move" ? () => setMovingIndex(i) : undefined
+              }
+            />
           ))}
         </svg>
       </div>
